a89780689104856ccc6b7e61fc61ffd2c7ae30c4
sebassmtz <you@example.com>
2024-04-29 19:04:31 -0500

    Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/example.lua Add .config/nvim/stylua.toml

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,4 @@
+-- boottrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.opt.timeoutlen = 1000
+vim.opt.ttimeoutlen = 0
---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Position cursor at the middle of the screen after scrolling half page
+vim.keymaps.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Map Ctrl+b in insert mode to delete to the end of the word without leaving insert mode
+vim.keymap.set("i", "<C-b>", "<C-o>de")
