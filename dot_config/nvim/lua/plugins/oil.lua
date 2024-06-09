eec9ebd3d18b9c4ba969c09a9dbedf44a5981be3
sebassmtz <sebastian01973@gmail.com>
2024-06-09 15:53:46 -0500

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/plugins/colorsheme.lua Add .config/nvim/lua/plugins/copilot.lua Add .config/nvim/lua/plugins/oil.lua Add .config/nvim/lua/plugins/telescope.lua

---
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    columns = { "icon" },
+    view_options = {
+      show_hidden = true,
+    },
+  },
+}
