3775619938606dd9b01cefb561f751a3f8176e81
sebassmtz <sebastian01973@gmail.com>
2024-06-11 20:35:53 -0500

    Update .config/nvim/lua/config/lazy.lua

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "plugins" },
+    { import = "plugins.lsp" },
   },
   defaults = {
     lazy = false,
