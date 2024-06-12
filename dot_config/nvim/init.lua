ab48acc5e2cba33468ba53a5cdc9f1d2bb26398c
sebassmtz <sebastian01973@gmail.com>
2024-06-11 20:34:27 -0500

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,4 +1,19 @@
--- boottrap lazy.nvim, LazyVim and your plugins
+--                                     ██
+--                                    ░░
+--  ███████   █████   ██████  ██    ██ ██ ██████████
+-- ░░██░░░██ ██░░░██ ██░░░░██░██   ░██░██░░██░░██░░██
+--  ░██  ░██░███████░██   ░██░░██ ░██ ░██ ░██ ░██ ░██
+--  ░██  ░██░██░░░░ ░██   ░██ ░░████  ░██ ░██ ░██ ░██
+--  ███  ░██░░██████░░██████   ░░██   ░██ ███ ░██ ░██
+-- ░░░   ░░  ░░░░░░  ░░░░░░     ░░    ░░ ░░░  ░░  ░░
+--
+--  ▓▓▓▓▓▓▓▓▓▓
+-- ░▓ author ▓ sebassmtz <ig @sebassmtz01>
+-- ░▓ page   ▓ https://sm-portfolio-dun.vercel.app/en
+-- ░▓ mirror ▓ https://github.com/sebassmtz/dotfiles-wsl
+-- ░▓▓▓▓▓▓▓▓▓▓
+-- ░░░░░░░░░░
+
 require("config.lazy")
 vim.opt.timeoutlen = 1000
 vim.opt.ttimeoutlen = 0
